9c56a4a58284a7acf7df462c287ae6b99cdf55a2
Ben Gianni <bgianni576@gmail.com>
2020-05-25 14:36:43 -0400

    more stuff

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 /*
-function sayHello(name) {
-    console.log("Hello " + name);
-}
+let sayHello = (name, name2) => {
+    return console.log('hello ' + name + ' ' + name2);
+};
+sayHello('Ben', 'Joe');
 */
+const logger = require('./logger');
+const os = require('os');
+const fs = require('fs');
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
 
-var sayHello = name => console.log('hello ' + name);
+fs.readdir('./', (err, files) => {
+    logger(files);
+});
 
-sayHello('Ben');
+//register a listener
+myEmitter.on('messageLogged', (message) => {
+    logger(message);
+});
+myEmitter.on('error', (message) => {
+    logger(message);
+})
+
+
+//signal an event has happened
+myEmitter.emit('messageLogged', 'hey there');
+myEmitter.emit('error', 'uh oh');
+
---
+++ logger.js
@@ -0,0 +1,6 @@
+var url = 'http://localhost:8315/logMessage';   
+
+var log = message => console.log(message);
+
+module.exports = log;
+//module.exports.endpoint = url;
